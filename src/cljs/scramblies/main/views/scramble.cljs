4a55663e784b8575b0bfb2c361558888b0073a06
Sandu Dorogan <sandu.dorogan@gmail.com>
2021-07-21 22:48:06 +0300

    Made the inputs more efficient, to be able to work on automated tests.

--- src/cljs/scramblies/main/views/scramble.cljs
+++ src/cljs/scramblies/main/views/scramble.cljs
@@ -1,6 +1,7 @@
 (ns scramblies.main.views.scramble
   (:require
-   [re-frame.core :as rf]))
+   [re-frame.core :as rf]
+   [reagent.core :as r]))
 
 (defn scramble-result []
   (let [result @(rf/subscribe [:scramble/result])]
@@ -10,12 +11,16 @@
          "Hooray! You can create the second string using the characters from the first one!"
          "Sadly, you can't create the second string using the characters from the first one. :(")])))
 
-(defn scramble-input [input-key]
-  (let [input-value (rf/subscribe [(->> input-key name (str "scramble/") keyword)])]
-    [:div
-     [:input
-      {:value     @input-value
-       :on-change #(rf/dispatch [:scramble/assoc-input-value input-key (.. % -target -value)])}]]))
+(defn scramble-input [_]
+  (let [input-value (r/atom nil)]
+    (fn [input-key]
+      [:div
+       [:input
+        {:value     @input-value
+         :on-change (fn [e]
+                      (let [value (.. e -target -value)]
+                        (reset! input-value value)
+                        (rf/dispatch [:scramble/assoc-input-value input-key value])))}]])))
 
 (defn scramble []
   [:div.grid-center.grid-5-rows

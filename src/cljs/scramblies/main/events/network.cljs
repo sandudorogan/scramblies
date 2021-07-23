985f0dc4ce15ebf399f431eec6ee8e461b38a660
Sandu Dorogan <sandu.dorogan@gmail.com>
2021-07-23 15:45:17 +0300

    Code cleanup.

--- src/cljs/scramblies/main/events/network.cljs
+++ src/cljs/scramblies/main/events/network.cljs
@@ -1,24 +1,18 @@
 (ns scramblies.main.events.network
-  (:require-macros [cljs.core.async.macros :refer [go go-loop]])
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [re-frame.core :as rf]
             [cljs.core.async :as async :refer [<!]]
             [cljs-http.client :as http]))
 
 (rf/reg-event-fx
   :make-request
-  (fn [{:keys [db]} [_ {:keys [custom-opts
-                              loader
-                              method
-                              on-success
-                              on-error
-                              progress-callback
-                              url]
-                       :as   params}]]
+  (fn [_ [_ {:keys [custom-opts
+                   method
+                   on-success
+                   on-error
+                   url]}]]
     (let [cancel     (async/chan)
           progress   (async/chan)
-          loader     (if (false? loader)
-                       #()
-                       #(rf/dispatch [:ui/loading (assoc params :cancel cancel)]))
           action     (case method
                        "GET"  http/get
                        "POST" http/post
@@ -35,17 +29,7 @@
                        (update :headers dissoc "Content-Type"))
           on-error   (or on-error #())
           on-success (or on-success #())]
-      (loader)
-      (if progress-callback
-        (go-loop []
-          (when-let [response (<! progress)]
-            (progress-callback response)
-            (if (= (:total response) (:loaded response))
-              (async/close! progress)
-              (recur))))
-        (async/close! progress))
       (go (when-let [response (<! (action url opts))]
-            (loader)
             (if (= 200 (:status response))
               (on-success response)
               (on-error response))))

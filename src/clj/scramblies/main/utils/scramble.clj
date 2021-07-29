b3c1e887b59d299641f439dd91a18c26ac9d0519
Sandu Dorogan <sandu.dorogan@gmail.com>
2021-07-29 10:37:20 +0300

    Parallel scrambles

--- src/clj/scramblies/main/utils/scramble.clj
+++ src/clj/scramblies/main/utils/scramble.clj
@@ -8,14 +8,33 @@
             (update map char #(inc (or % 0))))
           {} str))
 
-(defn scramble?
+(defn sequential-scramble?
   "Returns true if a portion of str1 characters
   can be rearranged to match str2, otherwise returns false."
   [a b]
-  ;; TODO: compute the alphabets in different threads
   (let [str1-alphabet (alphabet a)
         str2-alphabet (alphabet b)]
     (->> (map (fn [[letter usage]]
                 (>= (get str1-alphabet letter 0) usage))
               str2-alphabet)
          (every? true?))))
+
+(defn scramble?
+  "Returns true if a portion of str1 characters
+  can be rearranged to match str2, otherwise returns false."
+  [a b]
+  (let [str1-alphabet (future (alphabet a))
+        str2-alphabet (future (alphabet b))]
+    (->> (map (fn [[letter usage]]
+                (>= (get @str1-alphabet letter 0) usage))
+              @str2-alphabet)
+         (every? true?))))
+
+(comment
+  (def string (take 10000000 (cycle "345")))
+  (def string1 (take 10000000 (cycle "dfg")))
+
+  (time (scramble? "ion" "ionel"))
+  (time (sequential-scramble? "ion" "ionel"))
+  (time (sequential-scramble? string string))
+  (time (scramble? string1 string1)) )
